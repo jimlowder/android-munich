f4fb29a26dcffd31665366e0b3c2005f88437ae1
kai.roemer <kai.roemer@ac201b9c-1cc4-11df-94c5-13ecbea42b98>
2010-02-20 13:01:57 +0000

    client comm classes

---
+++ trunk/hackathon/russianchat/src/ru/roulette/comm/Identity.java
@@ -0,0 +1,22 @@
+package ru.roulette.comm;
+
+public class Identity {
+	private int id;
+	private byte[] image;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public byte[] getImage() {
+		return image;
+	}
+
+	public void setImage(byte[] image) {
+		this.image = image;
+	}
+}
