a0f4f3e72c33926fa438fa67590eac85ea0b069a
mr0738 <mr0738@mro.name>
2010-02-20 14:57:41 +0000

    kein Overwrite!

--- hackathon/pongjour-game/Pongjour/src/net/pongjour/model/GameEngineImpl.java
+++ hackathon/pongjour-game/Pongjour/src/net/pongjour/model/GameEngineImpl.java
@@ -16,92 +16,35 @@ class GameEngineImpl implements GameEngine {
 	GameEngineImpl(final SocketAddress addr) {
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see net.pongjour.model.GameEngine#getBallPosition()
-	 */
-	@Override
 	public PointF getBallPosition() {
-		// TODO Auto-generated method stub
-		return null;
+		throw new RuntimeException("Not implemented yet!");
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see net.pongjour.model.GameEngine#getMyRacketPosition()
-	 */
-	@Override
 	public float getMyRacketPosition() {
-		// TODO Auto-generated method stub
-		return 0;
+        throw new RuntimeException("Not implemented yet!");
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see net.pongjour.model.GameEngine#getMyScore()
-	 */
-	@Override
 	public int getMyScore() {
-		// TODO Auto-generated method stub
-		return 0;
+        throw new RuntimeException("Not implemented yet!");
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see net.pongjour.model.GameEngine#getOpponentRacketPosition()
-	 */
-	@Override
 	public float getOpponentRacketPosition() {
-		// TODO Auto-generated method stub
-		return 0;
+        throw new RuntimeException("Not implemented yet!");
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see net.pongjour.model.GameEngine#getOpponentScore()
-	 */
-	@Override
 	public int getOpponentScore() {
-		// TODO Auto-generated method stub
-		return 0;
+        throw new RuntimeException("Not implemented yet!");
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see net.pongjour.model.GameEngine#getRacketWidth()
-	 */
-	@Override
 	public float getRacketWidth() {
-		// TODO Auto-generated method stub
-		return 0;
+        throw new RuntimeException("Not implemented yet!");
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see net.pongjour.model.GameEngine#setMyRacketPosition(float)
-	 */
-	@Override
 	public void setMyRacketPosition(final float p) {
-		// TODO Auto-generated method stub
-
+        throw new RuntimeException("Not implemented yet!");
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see net.pongjour.model.GameEngine#startGame()
-	 */
-	@Override
 	public void startGame() {
-		// TODO Auto-generated method stub
-
+        throw new RuntimeException("Not implemented yet!");
 	}
-
 }
