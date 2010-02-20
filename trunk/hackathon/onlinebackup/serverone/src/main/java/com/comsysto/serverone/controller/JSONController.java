2d3e29aa46c59c8eb0ab18c7845b1c0c3800937c
jan68655 <jan68655@ac201b9c-1cc4-11df-94c5-13ecbea42b98>
2010-02-20 14:44:57 +0000

    first rest request provided

--- trunk/hackathon/onlinebackup/serverone/src/main/java/com/comsysto/serverone/controller/JSONController.java
+++ trunk/hackathon/onlinebackup/serverone/src/main/java/com/comsysto/serverone/controller/JSONController.java
@@ -1,19 +1,34 @@
 package com.comsysto.serverone.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.comsysto.vooone.Contact;
 
 /**
  * @author danielbartl
  * 
  */
 @Controller
-@RequestMapping("/json")
+@RequestMapping("/rest")
 public class JSONController {
 
-	@RequestMapping("hello")
-	public String hello() {
-		throw new IllegalStateException("Not implemented yet");
-	}
+	@RequestMapping(value = "contacts", method = RequestMethod.GET)
+	public @ResponseBody
+	List<Contact> getContacts() {
+		List<Contact> contacts = new ArrayList<Contact>();
+		Contact c1 = new Contact();
+		c1.setFirstName("Max");
+		c1.setLastName("Muster");
+		c1.seteMail("max.muster@gmail.com");
+		c1.setPhoneNumber("+ 49 170 123456");
+		contacts.add(c1);
+		return contacts;
 
+	}
 }
