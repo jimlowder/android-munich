0f2d5fc0716f56a4301c0b2171a6f0fa0481d673
jan68655 <jan68655@ac201b9c-1cc4-11df-94c5-13ecbea42b98>
2010-02-20 15:49:32 +0000

    id for contact introduced

--- trunk/hackathon/onlinebackup/voone/src/main/java/com/comsysto/vooone/Contact.java
+++ trunk/hackathon/onlinebackup/voone/src/main/java/com/comsysto/vooone/Contact.java
@@ -4,40 +4,67 @@ package com.comsysto.vooone;
  * Pojo für unseren Kontakt
  * 
  * @author stefandjurasic
- *
+ * 
  */
 public class Contact {
 
+	private Long id;
 	private String firstName;
 	private String lastName;
 	private String eMail;
 	private String phoneNumber;
-	
+
+	public Contact() {
+	}
+
+	public Contact(Long id, String firstName, String lastName, String eMail,
+			String phoneNumber) {
+		super();
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.eMail = eMail;
+		this.phoneNumber = phoneNumber;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
 	public String getFirstName() {
 		return firstName;
 	}
+
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	}
+
 	public String getLastName() {
 		return lastName;
 	}
+
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
+
 	public String geteMail() {
 		return eMail;
 	}
+
 	public void seteMail(String eMail) {
 		this.eMail = eMail;
 	}
+
 	public String getPhoneNumber() {
 		return phoneNumber;
 	}
+
 	public void setPhoneNumber(String phoneNumber) {
 		this.phoneNumber = phoneNumber;
 	}
-	
-	
-	
+
 }
