a2fb1e048defb5cc20425060cd6b57decc90d397
jan68655 <jan68655@ac201b9c-1cc4-11df-94c5-13ecbea42b98>
2010-02-20 15:49:00 +0000

    more rest api provided

--- hackathon/onlinebackup/serverone/src/main/java/com/comsysto/serverone/controller/JSONController.java
+++ hackathon/onlinebackup/serverone/src/main/java/com/comsysto/serverone/controller/JSONController.java
@@ -1,13 +1,18 @@
 package com.comsysto.serverone.controller;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
+import org.apache.commons.lang.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.comsysto.serverone.repository.DataSource;
 import com.comsysto.vooone.Contact;
 
 /**
@@ -17,6 +22,9 @@ import com.comsysto.vooone.Contact;
 @Controller
 @RequestMapping("/rest")
 public class JSONController {
+
+	@Autowired
+	private DataSource dataSource;
 
 	@RequestMapping(value = "contacts/mock", method = RequestMethod.GET)
 	public @ResponseBody
@@ -31,12 +39,53 @@ public class JSONController {
 		return contacts;
 	}
 
+	@RequestMapping(value = "contact/mock", method = RequestMethod.GET)
+	public @ResponseBody
+	Collection<Contact> updateMock(
+			@RequestParam("id") Long id,
+			@RequestParam(value = "firstName", required = false) String firstName,
+			@RequestParam(value = "lastName", required = false) String lastName,
+			@RequestParam("eMail") String eMail,
+			@RequestParam(value = "phoneNumber", required = false) String phoneNumber) {
+		return update(id, firstName, lastName, eMail, phoneNumber);
+	}
+
+	@RequestMapping(value = "contact", method = RequestMethod.PUT)
+	public @ResponseBody
+	Collection<Contact> update(
+			@RequestParam("id") Long id,
+			@RequestParam(value = "firstName", required = false) String firstName,
+			@RequestParam(value = "lastName", required = false) String lastName,
+			@RequestParam("eMail") String eMail,
+			@RequestParam(value = "phoneNumber", required = false) String phoneNumber) {
+		Contact contact = new Contact(id, firstName, lastName, eMail,
+				phoneNumber);
+		dataSource.put(contact);
+		return dataSource.all();
+	}
+
 	@RequestMapping(value = "contacts", method = RequestMethod.GET)
 	public @ResponseBody
-	List<Contact> getContacts() {
-		List<Contact> contacts = new ArrayList<Contact>();
-		return contacts;
+	Collection<Contact> all() {
+		return dataSource.all();
+	}
 
+	@RequestMapping(value = "contact/id", method = RequestMethod.GET)
+	public @ResponseBody
+	Contact get(@RequestParam("id") Long id) {
+		return dataSource.get(id);
+	}
+
+	@RequestMapping(value = "contact/email", method = RequestMethod.GET)
+	public @ResponseBody
+	Contact find(@RequestParam("eMail") String eMail) {
+		Collection<Contact> all = dataSource.all();
+		for (Contact contact : all) {
+			if (StringUtils.equals(eMail, contact.geteMail())) {
+				return contact;
+			}
+		}
+		return null;
 	}
 
 }
