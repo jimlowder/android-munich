f893307f26056d5e5102a0912799da58ba64e1db
jan68655 <jan68655@ac201b9c-1cc4-11df-94c5-13ecbea42b98>
2010-02-20 13:29:32 +0000

    stefans commit

---
+++ hackathon/onlinebackup/voone/src/main/java/com/comsysto/vooone/Contact.java
@@ -0,0 +1,43 @@
+package com.comsysto.vooone;
+
+/**
+ * Pojo für unseren Kontakt
+ * 
+ * @author stefandjurasic
+ *
+ */
+public class Contact {
+
+	private String firstName;
+	private String lastName;
+	private String eMail;
+	private String phoneNumber;
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String geteMail() {
+		return eMail;
+	}
+	public void seteMail(String eMail) {
+		this.eMail = eMail;
+	}
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	
+	
+	
+}
