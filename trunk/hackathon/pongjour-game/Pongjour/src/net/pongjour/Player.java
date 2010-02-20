84d040cec0cac49aac9ea4322cf31027f9602dc7
anjo@krank.net <anjo@krank.net@ac201b9c-1cc4-11df-94c5-13ecbea42b98>
2010-02-20 16:40:56 +0000

    first cut of network discovery

---
+++ trunk/hackathon/pongjour-game/Pongjour/src/net/pongjour/Player.java
@@ -0,0 +1,68 @@
+package net.pongjour;
+
+import java.util.Hashtable;
+
+public class Player {
+
+	private String name;
+	private String host;
+	private int port;
+	private int score;
+	private static Player self;
+	
+	public Player(String name, String host) {
+		this.name = name;
+		this.host = host;
+		this.score = 0;
+		this.port = 8080;
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+	public String getHost() {
+		return host;
+	}
+
+	public int getPort() {
+		return port;
+	}
+
+	public int getScore() {
+		return score;
+	}
+
+	public Hashtable getServiceInfo() {
+		Hashtable<String, Object> props = new Hashtable();
+		props.put("name", name);
+		props.put("host", host);
+		props.put("score", score+"");
+		props.put("port", port+"");
+		return props;
+	}
+	
+	@Override
+	public boolean equals(Object o) {
+		if (o instanceof Player) {
+			Player player = (Player) o;
+			return player.host.equals(host) && player.name.equals(name); 
+		}
+		return false;
+	}
+	
+	public static Player fromServiceInfo(Hashtable<String, String> props) {
+		Player player = new Player(props.get("name"), props.get("host"));
+		player.port = Integer.valueOf(props.get("port"));
+		player.score = Integer.valueOf(props.get("score"));
+		return player;
+	}
+	
+	public static void register(String name) {
+		self = new Player(name, Util.getLocalIpAddress());
+	}
+	
+	public static Player self() {
+		return self;
+	}
+}
--- trunk/hackathon/pongjour-game/Pongjour/src/net/pongjour/Pongjour.java
+++ trunk/hackathon/pongjour-game/Pongjour/src/net/pongjour/Pongjour.java
@@ -4,10 +4,28 @@ import android.app.Activity;
 import android.os.Bundle;
 
 public class Pongjour extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+
+	private NetworkHandler _networkHandler;
+	
+	private Player _player;
+
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+		registerNetwork();
+	}
+
+	private void registerNetwork() {
+//		WifiManager wifi = (WifiManager)getSystemService(Context.WIFI_SERVICE);
+//		MulticastLock lock = wifi.createMulticastLock("mylock");
+//		lock.acquire();
+
+		Player.register("my-name-" + (System.currentTimeMillis() % 100));
+		_networkHandler = new NetworkHandlerImp();
+//		lock.release();
+	}
+	
+	
 }
