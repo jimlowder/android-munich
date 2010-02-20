28776d8495c08eb4c70c86a508c0f6ba5a3c17f3
anjo <anjo@krank.net>
2010-02-20 16:40:56 +0000

    first cut of network discovery

---
+++ hackathon/pongjour-game/Pongjour/src/net/pongjour/Util.java
@@ -0,0 +1,28 @@
+package net.pongjour;
+
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.util.Enumeration;
+
+import android.util.Log;
+
+public class Util {
+
+	public static String getLocalIpAddress() {
+		try {
+			for (Enumeration<NetworkInterface> en = NetworkInterface.getNetworkInterfaces(); en.hasMoreElements();) {
+				NetworkInterface intf = en.nextElement();
+				for (Enumeration<InetAddress> enumIpAddr = intf.getInetAddresses(); enumIpAddr.hasMoreElements();) {
+					InetAddress inetAddress = enumIpAddr.nextElement();
+					if (!inetAddress.isLoopbackAddress()) {
+						return inetAddress.getHostAddress().toString();
+					}
+				}
+			}
+		} catch (SocketException ex) {
+			Log.e("Pongjour", ex.toString());
+		}
+		return null;
+	}
+}
