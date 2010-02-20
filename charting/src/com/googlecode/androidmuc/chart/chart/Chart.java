e410e59a28efd25527fca8576b0d48053fd5d088
junzhangdev <junzhangdev@ac201b9c-1cc4-11df-94c5-13ecbea42b98>
2010-02-20 17:49:15 +0000

    add paintText(Canvas c, String text, int beginDrawX, int beginDrawY)

--- charting/src/com/googlecode/androidmuc/chart/chart/Chart.java
+++ charting/src/com/googlecode/androidmuc/chart/chart/Chart.java
@@ -2,6 +2,10 @@ package com.googlecode.androidmuc.chart.chart;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Paint.FontMetrics;
+import android.graphics.Paint.Style;
 import android.util.AttributeSet;
 import android.view.View;
 
@@ -28,5 +32,15 @@ public abstract class Chart extends View {
 		this.title = title;
 	}
 	
+	public void paintText(Canvas c, String text, int beginDrawX, int beginDrawY) {
+		Paint foreground = new Paint(Paint.ANTI_ALIAS_FLAG);
+		foreground.setColor(Color.WHITE);
+		foreground.setStyle(Style.FILL);
+		foreground.setTextSize(25);
+		foreground.setTextAlign(Paint.Align.CENTER);
+		FontMetrics fm = foreground.getFontMetrics();
+		
+		c.drawText(text, beginDrawX, beginDrawY, foreground);
+	}
 	
 }
