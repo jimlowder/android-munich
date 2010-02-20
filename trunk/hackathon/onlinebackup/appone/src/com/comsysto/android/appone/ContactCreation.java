e27bc3ac1403ea7d499e428492e7a87ebe660ae2
jan68655 <jan68655@ac201b9c-1cc4-11df-94c5-13ecbea42b98>
2010-02-20 14:18:24 +0000

    added new layout files & activities

---
+++ trunk/hackathon/onlinebackup/appone/src/com/comsysto/android/appone/ContactCreation.java
@@ -0,0 +1,18 @@
+package com.comsysto.android.appone;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+
+public class ContactCreation extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.createcontactview);
+        
+    }
+}
--- trunk/hackathon/onlinebackup/appone/src/com/comsysto/android/appone/StartActivity.java
+++ trunk/hackathon/onlinebackup/appone/src/com/comsysto/android/appone/StartActivity.java
@@ -1,15 +1,28 @@
 package com.comsysto.android.appone;
 
+import com.comsysto.android.appone.*;
+
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class StartActivity extends Activity {
-
+	
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
-	}
+		Button findViewById = (Button)findViewById(R.id.ButtonGoToCreate);
+		findViewById.setOnClickListener(new View.OnClickListener() {
+			
+			public void onClick(View arg0) {
+				Intent var = new Intent(StartActivity.this, ContactCreation.class);
+				startActivity(var);
+			}
+		});
 		
+	}
 }
