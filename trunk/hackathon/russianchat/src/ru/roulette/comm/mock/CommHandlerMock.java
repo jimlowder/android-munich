320214bbbd277b2e79a859ac6f789bba7ec1942d
michi.karas <michi.karas@ac201b9c-1cc4-11df-94c5-13ecbea42b98>
2010-02-21 21:57:31 +0000

    removed some @Overrides

--- trunk/hackathon/russianchat/src/ru/roulette/comm/mock/CommHandlerMock.java
+++ trunk/hackathon/russianchat/src/ru/roulette/comm/mock/CommHandlerMock.java
@@ -1,6 +1,5 @@
 package ru.roulette.comm.mock;
 
-import ru.roulette.chat.R;
 import ru.roulette.comm.CommHandler;
 import ru.roulette.comm.Identity;
 
@@ -12,23 +11,23 @@ public class CommHandlerMock implements CommHandler {
 		// TODO Auto-generated constructor stub
 	}
 
-	@Override
+	
 	public String getMyMessages(int myid) {
 		return this.lastMsg;
 	}
 
-	@Override
+	
 	public int login(byte[] image) {
 		return 4711;
 	}
 
-	@Override
+	
 	public void message(int myid, int destid, String message) {
 		this.lastMsg = message;
 
 	}
 
-	@Override
+	
 	public Identity nextIdentity() {
 		Identity identity = new Identity();
 		identity.setId(4712);
@@ -39,7 +38,7 @@ public class CommHandlerMock implements CommHandler {
 		return identity;
 	}
 
-	@Override
+	
 	public void logoff(int myid) {
 		// TODO Auto-generated method stub
 		
